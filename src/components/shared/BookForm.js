dcd365b445cb68a7db82f5dbfa9467bd1469f8f0
NikPurvis <nikpurvis@gmail.com>
2022-05-01 09:25:04 -0600

    Add Book form fleshed out

--- src/components/shared/BookForm.js
+++ src/components/shared/BookForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Form, Container, Button } from 'react-bootstrap'
+import React, { useState } from "react"
+import { Form, Container, Button } from "react-bootstrap"
 
 const BookForm = (props) => {
     const {book, handleChange, handleSubmit, heading} = props
@@ -8,7 +8,67 @@ const BookForm = (props) => {
         <Container className="justify-content-center">
             <h3>{heading}</h3>
 
-            </Container>
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                    placeholder="Book title"
+                    value={book.title}
+                    name="title"
+                    onChange={handleChange}
+            />
+
+                <Form.Label>Author</Form.Label>
+                <Form.Control
+                    placeholder="Name of author"
+                    value={book.author}
+                    name="author"
+                    onChange={handleChange}
+            />
+
+                <Form.Label>Publication</Form.Label>
+                    <Form.Control
+                        placeholder="Publication Year"
+                        value={book.publication}
+                        name="publication"
+                        onChange={handleChange}
+                />
+
+                <Form.Label>Cover</Form.Label>
+                    <Form.Control
+                        placeholder="Cover image"
+                        value={book.cover}
+                        name="cover"
+                        onChange={handleChange}
+                />
+
+                <Form.Label>ISBN</Form.Label>
+                    <Form.Control
+                        placeholder="ISBN Number"
+                        value={book.isbn}
+                        name="isbn"
+                        onChange={handleChange}
+                />
+                
+                <Form.Label>Genre</Form.Label>
+                    <Form.Control
+                        placeholder="Book genre"
+                        value={book.genre}
+                        name="genre"
+                        onChange={handleChange}
+                />
+
+                <Form.Label>Description</Form.Label>
+                    <Form.Control
+                        placeholder="Description of book (think back of the book blurb - no spoilers!)"
+                        value={book.description}
+                        name="description"
+                        onChange={handleChange}
+                />
+
+                <Button type="submit">Submit</Button>
+            </Form>
+
+        </Container>
     )
 }
 
