4150cf113f0d7fead6a33c4c02ca6055a4168fc4
NikPurvis <nikpurvis@gmail.com>
2022-05-01 20:52:16 -0600

    Edit only appearing for profile owner

--- src/components/profile/ShowProfile.js
+++ src/components/profile/ShowProfile.js
@@ -21,9 +21,6 @@ const ShowProfile = (props) => {
         id = user._id
     }
 
-    console.log("====================")
-    console.log("WAY UP HERE uid", id)
-    
     useEffect(() => {
         showProfile(id)
             .then(profile => {
@@ -45,70 +42,49 @@ const ShowProfile = (props) => {
         )
     }
 
-    // // Setting a boolean to determine if the ability to edit will render.
-    let theirProfile = false
-    // const idTest = user._id
-
-    // if (id !== null) {console.log("nope see:", id, user.profile.owner)
-    // } else {
-    //     console.log("totally null")
-    // }
-
-    // if (user._id === null || user._id === undefined) {
-    //     theirProfile = false
-    // } else {
-    //     if (user._id !== user.profile.owner) {
-    //         theirProfile = true
-    //     }
-    // }
-    
-    // console.log("******************")
-    // console.log("user id:", id)
-    // console.log("profile owner:", profile.owner)
-    // console.log("username:", profile.username)
-    // console.log("******************")
-
     // What to do with data from the API call
     return (
-        <><br />
-        <Container className="fluid">
-            <Card className="text-info bg-dark">
-                <Card.Header className="display-4">
-                        Your User Profile
-                </Card.Header>
-                <Card.Body>
-                    <Card.Text>
-                        <Card.Header>Username: {profile.username}</Card.Header><br/>
-                        <Card.Header>Location: {profile.location}</Card.Header><br/>
-                        <Card.Header>Favorite Books: {profile.fav_books}</Card.Header><br/>
-                        <Card.Header>Favorite Authors: {profile.fav_authors}</Card.Header><br/>
-                        <Card.Header>Favorite genres: {profile.genres}</Card.Header><br/>
-                        <Card.Header>Favorite quote: {profile.quote}</Card.Header><br/>
-                    </Card.Text>
+        <>
+            <br />
+            <Container className="fluid">
+                <Card className="text-info bg-dark">
+                    <Card.Header className="display-4">
+                            Your User Profile
+                    </Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            <Card.Header>Username: {profile.username}</Card.Header><br/>
+                            <Card.Header>Location: {profile.location}</Card.Header><br/>
+                            <Card.Header>Favorite Books: {profile.fav_books}</Card.Header><br/>
+                            <Card.Header>Favorite Authors: {profile.fav_authors}</Card.Header><br/>
+                            <Card.Header>Favorite genres: {profile.genres}</Card.Header><br/>
+                            <Card.Header>Favorite quote: {profile.quote}</Card.Header><br/>
+                        </Card.Text>
 
-                    {id === profile.owner &&
-                        <Button onClick={() => setModalOpen(true)} className="m-2" variant="success">
-                                Edit Profile
-                        </Button>
-                    }
+                        {/* Conditional to check if user is profile owner and render action buttons accordingly */}
+                        {user && (user._id === profile.owner)
+                            ?
+                                <>
+                                    <Button variant="success" size='sm' onClick={() => setModalOpen(true)}>
+                                        Edit Profile
+                                    </Button>
+                                </>
+                            :
+                                null
+                        }<br/>
+                    </Card.Body>
+                </Card>
+            </Container>
 
-                    {/* <Button onClick={() => deleteThisBook()} className="m-2" variant="danger">
-                        Delete Book
-                    </Button> */}
-
-                </Card.Body>
-            </Card>
-        </Container>
-
-        { <EditProfileModal 
-                profile={profile}
-                show={modalOpen}
-                user={user}
-                triggerRefresh={() => setUpdated(prev => !prev)}
-                updateProfile={updateProfile}
-                handleClose={() => setModalOpen(false)}
-            />
-        }
+            { <EditProfileModal
+                    user={user}
+                    profile={profile}
+                    show={modalOpen}
+                    triggerRefresh={() => setUpdated(prev => !prev)}
+                    updateProfile={updateProfile}
+                    handleClose={() => setModalOpen(false)}
+                />
+            }
         </>
     )
 }
